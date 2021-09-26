3380d6760fcfac44c18ce5fe99385fd94e9dc8eb
kreetigulati <kreetigulati@outlook.com>
2021-09-26 09:49:58 -0700

    updating csv file and about.html page

--- app/app.py
+++ app/app.py
@@ -1,17 +1,21 @@
-from flask import Flask, jsonify, render_template
+import pandas as pd
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import tensorflow
+from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from keras.models import load_model
+import h5py
 
 
 app = Flask(__name__)
-
+model = load_model('mortgage_deep_learning17_3.h5')
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('index.html', title='Home Page')
-
-@app.route("/prediction")
-def prediction():
-    return render_template('prediction.html', title='Prediction')
 
 @app.route("/model")
 def model():
@@ -21,9 +25,33 @@ def model():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/prediction")
+def prediction():
+    return render_template('prediction.html', title='Prediction')
+
+@app.route('/prediction',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('prediction.html', title='Prediction')
+
 @app.route("/dashboard")
 def dashboard():
     return render_template('dashboard.html', title='Dashboard')
+
+@app.route('/dashboard',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
